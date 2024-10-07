4d98bd8aa67bd6e84cd63e2eb4ea296d291c5284
Brandon Arbour <arbour.brandon@gmail.com>
2024-10-07 14:48:32 -0400

    Configured ECS instance with httpd image

--- Terraform/Dev/ecs.tf
+++ Terraform/Dev/ecs.tf
@@ -1,3 +1,39 @@
 resource "aws_ecs_cluster" "dev_ecs_cluster" {
   name = "dev_ecs_cluster"
 }
+
+resource "aws_ecs_task_definition" "dev_task_def" {
+  family = "Web-Server"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu = 256
+  memory = 512
+  
+  container_definitions = jsonencode([
+    {
+      name = "Apache"
+      image = "httpd:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "dev_ecs_service" {
+  name = "httpd"
+  cluster = aws_ecs_cluster.dev_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.dev_task_def.id
+  launch_type = "FARGATE"
+  desired_count = 1
+
+  network_configuration {
+    subnets = aws_subnet.public_subnet[*].id
+    security_groups = [aws_security_group.allow_dev_access.id]
+    assign_public_ip = true
+  }
+}
--- Terraform/Dev/vpc.tf
+++ Terraform/Dev/vpc.tf
@@ -22,3 +22,17 @@ resource "aws_internet_gateway" "dev_igw" {
     Name = "dev_igw"
   }
 }
+
+resource "aws_route_table" "dev_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_igw.id
+  }
+}
+resource "aws_route_table_association" "dv_rta" {
+  count = 1
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.dev_rt.id
+}
