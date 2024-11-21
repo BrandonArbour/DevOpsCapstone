db031c98037de1154137bc7d63c80f94acd49f32
Brandon Arbour <arbour.brandon@gmail.com>
2024-11-21 14:26:02 -0500

    Testing TG changes

--- Terraform/Dev/lb.tf
+++ Terraform/Dev/lb.tf
@@ -21,6 +21,12 @@ resource "aws_lb_target_group" "dev_alb_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.dev_vpc.id
-  target_type = "ip"
-  depends_on = [aws_ecs_service.dev_ecs_service]  
+  health_check {
+    path = "/"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 3
+    unhealthy_threshold = 2
+    matcher = "200"
+  }
 }
